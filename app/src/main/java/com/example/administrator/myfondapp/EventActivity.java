d3f3dc5925babf75e1115b610d96b1e5d7ea33bf
USERCHI-57HL27M\Administrator <1432560150@qq.com>
2018-05-09 15:59:11 +0800

    new class

---
+++ app/src/main/java/com/example/administrator/myfondapp/EventActivity.java
@@ -0,0 +1,18 @@
+package com.example.administrator.myfondapp;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2018\5\9 0009.
+ */
+
+public class EventActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+}
